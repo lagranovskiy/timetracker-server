d748e099fe9147b3b5f44c439ea03b1a7584761a
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2015-03-19 13:40:48 +0100

    Coverage package removed from git

--- test/coverage/instrument/app/controller/Person.js
+++
@@ -1,9 +0,0 @@
-
-var __cov_M1yKbouHhYs_4EMEcnR8Zw = (Function('return this'))();
-if (!__cov_M1yKbouHhYs_4EMEcnR8Zw.__coverage__) { __cov_M1yKbouHhYs_4EMEcnR8Zw.__coverage__ = {}; }
-__cov_M1yKbouHhYs_4EMEcnR8Zw = __cov_M1yKbouHhYs_4EMEcnR8Zw.__coverage__;
-if (!(__cov_M1yKbouHhYs_4EMEcnR8Zw['app/controller/Person.js'])) {
-   __cov_M1yKbouHhYs_4EMEcnR8Zw['app/controller/Person.js'] = {"path":"app/controller/Person.js","s":{"1":0,"2":0,"3":0,"4":0,"5":0},"b":{},"f":{"1":0},"fnMap":{"1":{"name":"(anonymous_1)","line":19,"loc":{"start":{"line":19,"column":24},"end":{"line":19,"column":58}}}},"statementMap":{"1":{"start":{"line":6,"column":0},"end":{"line":6,"column":29}},"2":{"start":{"line":7,"column":0},"end":{"line":7,"column":30}},"3":{"start":{"line":8,"column":0},"end":{"line":8,"column":60}},"4":{"start":{"line":9,"column":0},"end":{"line":9,"column":46}},"5":{"start":{"line":19,"column":0},"end":{"line":28,"column":2}}},"branchMap":{}};
-}
-__cov_M1yKbouHhYs_4EMEcnR8Zw = __cov_M1yKbouHhYs_4EMEcnR8Zw['app/controller/Person.js'];
-__cov_M1yKbouHhYs_4EMEcnR8Zw.s['1']++;var async=require('async');__cov_M1yKbouHhYs_4EMEcnR8Zw.s['2']++;var _=require('underscore');__cov_M1yKbouHhYs_4EMEcnR8Zw.s['3']++;var PersonRepository=require('../model/PersonRepository');__cov_M1yKbouHhYs_4EMEcnR8Zw.s['4']++;var personRepository=new PersonRepository();__cov_M1yKbouHhYs_4EMEcnR8Zw.s['5']++;exports.getPersonData=function(request,response,next){__cov_M1yKbouHhYs_4EMEcnR8Zw.f['1']++;};
--- test/coverage/instrument/app/model/Person.js
+++
@@ -1,9 +0,0 @@
-
-var __cov_n4kbMgXROQJBC9ClCHd90w = (Function('return this'))();
-if (!__cov_n4kbMgXROQJBC9ClCHd90w.__coverage__) { __cov_n4kbMgXROQJBC9ClCHd90w.__coverage__ = {}; }
-__cov_n4kbMgXROQJBC9ClCHd90w = __cov_n4kbMgXROQJBC9ClCHd90w.__coverage__;
-if (!(__cov_n4kbMgXROQJBC9ClCHd90w['app/model/Person.js'])) {
-   __cov_n4kbMgXROQJBC9ClCHd90w['app/model/Person.js'] = {"path":"app/model/Person.js","s":{"1":0,"2":0,"3":0,"4":0,"5":0},"b":{},"f":{"1":0,"2":0,"3":0},"fnMap":{"1":{"name":"(anonymous_1)","line":5,"loc":{"start":{"line":5,"column":13},"end":{"line":5,"column":33}}},"2":{"name":"(anonymous_2)","line":11,"loc":{"start":{"line":11,"column":17},"end":{"line":11,"column":29}}},"3":{"name":"(anonymous_3)","line":19,"loc":{"start":{"line":19,"column":17},"end":{"line":19,"column":29}}}},"statementMap":{"1":{"start":{"line":5,"column":0},"end":{"line":24,"column":2}},"2":{"start":{"line":7,"column":4},"end":{"line":23,"column":6}},"3":{"start":{"line":12,"column":12},"end":{"line":12,"column":31}},"4":{"start":{"line":20,"column":12},"end":{"line":20,"column":33}},"5":{"start":{"line":25,"column":0},"end":{"line":25,"column":24}}},"branchMap":{}};
-}
-__cov_n4kbMgXROQJBC9ClCHd90w = __cov_n4kbMgXROQJBC9ClCHd90w['app/model/Person.js'];
-__cov_n4kbMgXROQJBC9ClCHd90w.s['1']++;var Person=function(dbPerson){__cov_n4kbMgXROQJBC9ClCHd90w.f['1']++;__cov_n4kbMgXROQJBC9ClCHd90w.s['2']++;return{getDbId:function(){__cov_n4kbMgXROQJBC9ClCHd90w.f['2']++;__cov_n4kbMgXROQJBC9ClCHd90w.s['3']++;return dbPerson.id;},getData:function(){__cov_n4kbMgXROQJBC9ClCHd90w.f['3']++;__cov_n4kbMgXROQJBC9ClCHd90w.s['4']++;return dbPerson.data;}};};__cov_n4kbMgXROQJBC9ClCHd90w.s['5']++;module.exports=Person;
