dd37c21d19b0837a03feaa5f745a33b72674289b
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2015-04-08 09:38:07 +0200

    newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['timetracker-server'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '81d71f9a2e68f79050c3b13f132877a7d82ab54b',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
