d442c29f784fc92616ad850e284d578fd8a07c67
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2015-04-08 13:06:12 +0200

    cookie handling improved

--- app/app.js
+++ app/app.js
@@ -44,11 +44,7 @@ app.set('trust proxy', 1) // trust first proxy
 // Configuring Passport
 app.use(expressSession({
     secret: config.sessionSecret,
-    proxy: true,
     cookie: {
-        secure: true,
-        domain: 'pac-timetracker.herokuapp.com',
-        httpOnly: false,
         maxAge: 24 * 60 * 60 * 1000
     }
 }));
