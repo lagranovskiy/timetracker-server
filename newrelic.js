cbadbdeaff754f23091845edb71e62f6b221ce0c
Leonid Agranovskiy <leonid.agranovskiy@prodyna.com>
2015-06-05 09:49:30 +0200

    new relic licence as env var

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : '81d71f9a2e68f79050c3b13f132877a7d82ab54b',
+  license_key : process.env.NEW_RELIC_LICENSE_KEY,
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
